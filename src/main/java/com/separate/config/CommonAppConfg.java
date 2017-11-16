7f86f300a86623de501015f00fc82d7247ba3ab9
divine <mefistofell081992gmail.com>
2017-11-16 16:15:20 +0200

    using Spring without config class

--- src/main/java/com/separate/config/CommonAppConfg.java
+++ src/main/java/com/separate/config/CommonAppConfg.java
@@ -1,9 +1,12 @@
 package com.separate.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
+//@ComponentScan(basePackages = "com.separate.config")
+// using ComponentScan we can find beans in packages without config class DON'T forget add @Component or etc to class
 @Import({SoapConfig.class, RestConfig.class})
 public class CommonAppConfg {
 }
